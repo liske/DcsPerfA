18ebad6eac766995cfc696654425a761bf3f0f10
Thomas Liske <thomas@fiasko-nw.net>
2013-05-05 23:53:56 +0200

    Prepare interactive plot requests.

--- public/js/DPAGraphs.js
+++ public/js/DPAGraphs.js
@@ -1,18 +1,35 @@
-var DPAGraph = new function DPAGraph(id, params) {
+function DPAGraph(pid, id, params) {
     this.id = id;
-    this.params = params;
 
     this.setParams = function(params) {
 	this.params = params;
+	this.setSrc();
     }
 
     this.getParams = function() {
 	return params;
     }
 
-    this.getHtml = function() {
-	return '<img id="' + id + '" />';
+    this.setSrc = function() {
+	$('#' + id).attr('src', 'plot/?params=' +
+	    encodeURIComponent(JSON.stringify(params)).replace(/[!'()]/g, escape).replace(/\*/g, "%2A") //'
+	);
     }
+
+    var close = $('<div/>', {
+	title: 'close',
+	class: 'close',
+	html: '<b>x</b>',
+    });
+    close.click(function() {
+	$('#div_' + id).remove();
+    });
+
+    $('<div/>', {
+	id: 'div_' + id,
+	class: 'drag',
+    }).appendTo(pid).draggable().append('<img id="' + id + '" />').append(close);
+    this.setParams(params);
 }
 
 var DPAGraphs = new function DPAGraphs() {
@@ -23,11 +40,11 @@ var DPAGraphs = new function DPAGraphs() {
 	return graphs[id];
     }
 
-    DPAGraphs.addGraph = function(params) {
+    DPAGraphs.addGraph = function(pid, params) {
 	var id = 'DPAGraph' + last_id++;
-	graphs[id] = new DPAGraph(id, params);
+	graphs[id] = new DPAGraph(pid, id, params);
 
-	return id;
+	return graphs[id];
     }
 
     DPAGraphs.updateGraph = function(id, params) {
