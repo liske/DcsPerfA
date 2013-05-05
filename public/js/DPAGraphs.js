b05ed2e96c36b81182a216e52f47e08e15eb6ffe
Thomas Liske <thomas@fiasko-nw.net>
2013-05-05 18:48:43 +0200

    Include DPAGraphs.js.

--- public/js/DPAGraphs.js
+++ public/js/DPAGraphs.js
@@ -2,11 +2,15 @@ var DPAGraph = new function DPAGraph(id, params) {
     this.id = id;
     this.params = params;
 
-    this.update = function(params) {
+    this.setParams = function(params) {
 	this.params = params;
     }
 
-    this.html = function() {
+    this.getParams = function() {
+	return params;
+    }
+
+    this.getHtml = function() {
 	return '<img id="' + id + '" />';
     }
 }
